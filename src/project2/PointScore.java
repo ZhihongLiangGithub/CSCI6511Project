997f41b52a98b4d461a6f7c47284c03639b5c81f
Zhihong <zhihongliang@gwmail.gwu.edu>
2018-02-25 18:56:59 -0500

    search space optimizations

---
+++ src/project2/PointScore.java
@@ -0,0 +1,27 @@
+package project2;
+
+public class PointScore {
+
+    private int i;
+    private int j;
+    private double score;
+
+    public PointScore(int i, int j, double score) {
+        this.i = i;
+        this.j = j;
+        this.score = score;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public int getJ() {
+        return j;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
+}
